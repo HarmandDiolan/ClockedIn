83ed4cd664d394b5bb361d81bce5c514bf7be695
HarmandDiolan <2201103870@student.buksu.edu.ph>
2025-04-15 11:32:28 +0800

    UI

---
+++ app/src/main/java/com/example/clockedin/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.clockedin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText email, password;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(v -> {
+            // You can add actual auth logic here
+            //startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+        });
+    }
+}
---
+++ app/src/main/java/com/example/clockedin/WelcomeActivity.java
@@ -0,0 +1,25 @@
+package com.example.clockedin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+public class WelcomeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(WelcomeActivity.this, LoginActivity.class));
+            finish();
+        }, 2000); // 2-second splash screen
+    }
+}
